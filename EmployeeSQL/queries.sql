f1f3fa98c41440080f1ee94a1074c659f73d1c51
Dasha <84484371+DasaSimo@users.noreply.github.com>
2021-08-01 23:05:10 -0400

    adding a query for each employee name and department name

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -22,5 +22,13 @@ JOIN dept_manager AS dm
 JOIN employees AS e
 	ON e.emp_no = dm.emp_no;
 
+--4. Query for the department of each employee with the following information:
+--   employee number, last name, first name, and department name 
 
+SELECT  de.emp_no, last_name, first_name, dep_name
+FROM  employees AS e
+JOIN dep_emp AS de
+	ON e.emp_no = de.emp_no
+JOIN departments AS d
+	ON de.dep_no = d.dept_no;
 
