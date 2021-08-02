0eb82641471e3676958406941a6d93fae6542cab
Dasha <84484371+DasaSimo@users.noreply.github.com>
2021-08-01 23:46:47 -0400

    adding the frequency count of employee last name

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -62,6 +62,13 @@ JOIN departments AS d
 	ON de.dep_no = d.dept_no
 	WHERE dep_name = 'Sales' OR dep_name = 'Development';
 
+--8. Query for the frequency count of employee last names,
+--   i.e., how many employees share each last name (in descending order)
+
+SELECT  last_name, COUNT(last_name) AS "Last name frequency count" 
+FROM employees
+GROUP BY last_name
+ORDER BY "Last name frequency count" DESC;
 
 
 
