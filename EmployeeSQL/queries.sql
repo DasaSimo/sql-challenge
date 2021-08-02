5d8890095f4b3227079748c37fd5940d88afb6ce
Dasha <84484371+DasaSimo@users.noreply.github.com>
2021-08-01 22:27:06 -0400

    adding a query for the employees hired in 1986

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -1,6 +1,15 @@
 --1.Query for: employee number, last name, first name, sex, and salary
 
 SELECT e.emp_no, last_name, first_name, sex, salary 
-	FROM employees AS e
-	JOIN salaries AS s
+FROM employees AS e
+JOIN salaries AS s
  	ON e.emp_no = s.emp_no; 
+
+
+--2.Query for: first name, last name, and hire date for employees who were hired in 1986
+
+SELECT first_name, last_name, hire_date
+FROM  employees
+WHERE EXTRACT(YEAR FROM hire_date) = 1986;
+
+
